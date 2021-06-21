98ea45c5d1e676c313d80177aad4ff8fe633eec3
Anthony.Camerlain <anthony.camerlain@gmail.com>
2021-06-21 09:36:09 -0400

    Modification WaterArmor + Water Script

---
+++ Assets/ColdWater.cs
@@ -0,0 +1,29 @@
+using MoreMountains.CorgiEngine;
+using UnityEngine;
+
+public class ColdWater : DamageOnTouch
+{
+    [SerializeField] string waterArmorID = "";
+
+    protected override void Awake()
+    {
+        if (string.IsNullOrWhiteSpace(waterArmorID))
+        {
+            Debug.LogError("Missing the ID of the water armor!");
+        }
+        base.Awake();
+    }
+
+    protected override void OnCollideWithDamageable(Health health)
+    {
+        var inventory = health.gameObject.GetComponent<CharacterInventory>();
+        if (inventory.MainInventory.GetQuantity(waterArmorID) > 0)
+        {
+            return;
+        }
+        else
+        {
+            base.OnCollideWithDamageable(health);
+        }
+    }
+}
